ca7d89856d1d25d6ac5e5daba668080b96f08c06
Pedr0P4 <pedropaulo062011@gmail.com>
2024-04-11 01:16:50 -0300

    Atualização do main.cpp

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
 
+using namespace std;
+
+template <typename T> class RegAst{
+    private:
+        unsigned int quant;
+        T* elements;
+    
+    public:
+        RegAst(){
+            this->quant = 0;
+            this->elements = new T[0];
+        }
+
+        void addE(T* Astronauta){
+            T* novoArray = new T*[quant+1];
+
+            for(int i=0;i<this->quant;i++){
+                novoArray[i] = this->elements[i];
+            }
+
+            novoArray[this->quant] = item;
+
+            delete this->elements;
+            this->elements = novoArray;
+
+            this->quant++;
+        }
+};
+
+class Astronaut{
+    private:
+        int idade;
+        string cpf;
+        string nome;
+    public:
+    
+
+
+};
+
 int main(){
+    RegAst<*Astronaut>
+
 
     return 0;
 }
