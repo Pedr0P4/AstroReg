f1267e745afca0aa1d962a29868c543eb48d4f11
Pedr0P4 <pedropaulo062011@gmail.com>
2024-04-12 22:19:00 -0300

    Melhoria na formatação do CPF

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,68 +1,80 @@
 #include <iostream>
 #include <limits>
+#include <string>
 
 using namespace std;
 
+//Classe para armazenar elementos de qualquer tipo (nesse caso do tipo "Astronauta").
 template <typename T> class RegAst{
     private:
-        unsigned int quant;
-        T* elements;
+        unsigned int quant; //Variável de quantidade de elementos.
+        T* elements; //Variável para os elementos.
     
     public:
+		//Construtor.
         RegAst(){
-            this->quant = 0;
-            this->elements = new T[0];
+            this->quant = 0; //A classe começa com 0 em quant.
+            this->elements = new T[0]; //A classe começa com um ponteiro de um array com 0 elementos.
         }
 
+		//Função para adicionar elementos.
         void addE(T ast){
-            T* novoArray = new T[quant+1];
+            T* novoArray = new T[quant+1]; //Criação de um ponteiro para um array do tamanho da quantidade de elementos atuais +1.
 
             for(int i=0;i<this->quant;i++){
-                novoArray[i] = this->elements[i];
+                novoArray[i] = this->elements[i]; //Copia todos os elementos de elements da classe para esse novo array.
             }
 
-            novoArray[this->quant] = ast;
+            novoArray[this->quant] = ast; //No fim do array adiciona o elemento novo. 
 
-            delete this->elements;
-            this->elements = novoArray;
+            delete this->elements; //Deleta o ponteiro dos elementos da classe.
+            this->elements = novoArray; //Os elementos da classe agora recebem o novo array com o novo elemento e os anteriores.
 
-            this->quant++;
+            this->quant++; //Quantidade de elementos é incrementado em 1.
         }
 
+		//Função para mostrar o elemento na posição recebida no parâmetro (pos).
 		T getAst(unsigned int pos){
 			return this->elements[pos];
 		}
 
+		//Função para retornar o valor da quantidade de elementos.
 		int getQuant(){
 			return this->quant;
 		}
 
+		//Destrutor.
 		~RegAst(){
 			delete[] elements;
 		}
 };
 
+//Classe com as informações de um astronauta.
 class Astronauta{
     private:
-        int idade;
-        string cpf;
-        string nome;
+        int idade; //Idade do astronauta.
+        string cpf; //CPF do astronauta.
+        string nome; //Nome do astronauta.
 
     public:
-    	Astronauta(int idade = 0, string cpf = "00000000000", string nome = "Pedro"){
-			this->idade = idade;
-			this->cpf = cpf;
-			this->nome = nome;
+		//Construtor.
+    	Astronauta(int idade, string cpf, string nome = "Pedro"){
+			this->idade = idade; //idade da classe recebe idade (parâmetro).
+			this->cpf = cpf; //cpf da classe recebe cpf (parâmetro).
+			this->nome = nome; //nome da classe recebe nome (parâmetro).
 		}
 
+		//Função que retorna o valor da variável idade da classe.
 		int getIdade(){
 			return this->idade;
 		}
 
+		//Função que retorna o conteúdo da variável cpf da classe.
 		string getCPF(){
 			return this->cpf;
 		}
 
+		//Função que retorna o conteúdo da variável nome da classe.
 		string getNome(){
 			return this->nome;
 		}
@@ -71,10 +83,19 @@ class Astronauta{
 };
 
 char* FormatCPF(string cpf){
-	unsigned int stam = cpf.length(); // Para pegar o tamanho da string cpf
-	unsigned int j = 0; // Para controlar o índice de cpf_f
-	char* cpf_f = new char[15];
+	unsigned int stam = cpf.length(); // Para pegar o tamanho da string cpf.
+	unsigned int j = 0; // Para controlar o índice de cpf_f.
+	char* cpf_f = new char[15]; //criação de um ponteiro para um array de char de tamanho 15.
 
+	if(stam < 11){
+		string temp(11 - stam, '0');
+		cpf += temp;
+		stam = cpf.length();
+	} else{
+		stam = 11;
+	}
+
+	//Loop para formatação da string e inserção no array de char.
 	for(int i=0;i<stam;i++){
 		if((i+1)%3 == 0 && i <= 6){
 			cpf_f[j++] = cpf[i];
@@ -86,6 +107,7 @@ char* FormatCPF(string cpf){
 			cpf_f[j++] = cpf[i];
 		}
 	}
+	cpf_f[j++] = '\0'; //Adição do caractere nulo ao fim do array.
 
 	return cpf_f;
 }
