fa87fa6b7552cca641363d9e91f68b361473c0d8
Pedro Paulo <pedropaulo062011@gmail.com>
2024-04-17 00:30:11 -0300

    Mudança de switch para if (evitar bugs) e implementação do comando 5

--- Source/main.cpp
+++ Source/main.cpp
@@ -182,12 +182,14 @@ Astronauta* addAstro(){
 
 Astronauta* getAstroByCPF(string cpf, Registros<Astronauta*> RegAst){
 	unsigned int qtastro = RegAst.getQuant();
+	Astronauta* astro;
 	for(int i=0;i<qtastro;i++){
 		string t_cpf = RegAst.getE(i)->getCPF();
 		if(cpf == t_cpf){
-			return;
+			astro = RegAst.getE(i);
 		}
 	}
+	return astro;
 }
 
 Voo* addVoo(){
@@ -197,6 +199,18 @@ Voo* addVoo(){
 	cin >> code_voo;
 
 	Voo* voo = new Voo(code_voo);
+	return voo;
+}
+
+Voo* getVooByCode(int code, Registros<Voo*> RegVoos){
+	unsigned int qtvoo = RegVoos.getQuant();
+	Voo* voo;
+	for(int i=0;i<qtvoo;i++){
+		int t_code = RegVoos.getE(i)->getCode();
+		if(code == t_code){
+			voo = RegVoos.getE(i);
+		}
+	}
 	return voo;
 }
 
@@ -226,115 +240,102 @@ int main(){
 												//^^^^^^^^^^^------> Função da classe do vetor que retorna a quantidade de elementos (astronautas).
 		unsigned int qt_voos = RegistroVoos.getQuant();
 		
-		switch(ans){
-			case 1:
-				//Chama a função addE (adiciona um elemento no vetor) e, como parâmetro, recebe a função addAstro (cria um ponteiro de Astronauta).
-				RegistroAstronautas.addE(addAstro());
+		if(ans == 1){
+			//Chama a função addE (adiciona um elemento no vetor) e, como parâmetro, recebe a função addAstro (cria um ponteiro de Astronauta).
+			RegistroAstronautas.addE(addAstro());
+			cout << endl;
+		} else if(ans == 2){
+			cout << "Astronautas:\n" << endl;
+
+			//Caso não haja elementos no vetor (astronautas), imprime uma mensagem de erro.
+			if(qtastro == 0){
+				cout << "Não há astronautas cadastrados!" << endl;
 				cout << endl;
-				break;
+			} else{ //Caso haja elementos no vetor (astronautas).
 
-			case 2:
-				cout << "Astronautas:\n" << endl;
+				string text = RegistroAstronautas.getE(0)->getNome(); //Variável que pegará o nome de cada astronauta.
+				unsigned int tamax = text.length(); //Variável para armazenar o maior tamanho de nome, começará com o tamanho do nome do primeiro astronauta do vetor.
+				unsigned int temptam; //Variável que carregará um tamanho temporário para que haja a comparação.
 
-				//Caso não haja elementos no vetor (astronautas), imprime uma mensagem de erro.
-				if(qtastro == 0){
-					cout << "Não há astronautas cadastrados!" << endl;
-					cout << endl;
-				} else{ //Caso haja elementos no vetor (astronautas).
-
-					string text = RegistroAstronautas.getE(0)->getNome(); //Variável que pegará o nome de cada astronauta.
-					unsigned int tamax = text.length(); //Variável para armazenar o maior tamanho de nome, começará com o tamanho do nome do primeiro astronauta do vetor.
-					unsigned int temptam; //Variável que carregará um tamanho temporário para que haja a comparação.
-
-					//Loop para que haja a comparação entre os tamanhos de cada elemento do vetor, pra no fim "tamax" carregar o tamanho do maior nome.
-					for(int i=0;i<qtastro;i++){
-						text = RegistroAstronautas.getE(i)->getNome();
-						temptam = text.length();
-						if(tamax < temptam){
-							tamax = temptam;
-						}
-					}
-
-					int cpftamanho = 14; //Variável que carrega o tamanho máximo de um CPF já formatado.
-					int tampadrao = 16; //Variável que carrega o tamanho padrão (tamanho do cpf + 2 para que haja um pequeno espaçamento (2)).
-					int tamtotal = (tamax-2) + tamax + cpftamanho + (cpftamanho/2) + 3*tampadrao; //Tamanho total da largura da tabela.
-
-					//Nomes das colunas.
-					cout << setw(tamax-2) << left << ""
-						 << setw(tamax+cpftamanho) << left << "Nome"
-						 << setw(tampadrao+(cpftamanho/2)) << left << "CPF"
-						 << setw(tampadrao) << left << "Idade"
-						 << setw(tampadrao) << left << "Situação"
-						 << endl;
-
-					//Linha para separar.
-					cout << setfill('-') << setw(tamtotal) << "-" << endl;
-					cout << setfill(' ');
-
-					//Print dos valores de cada astronauta.
-					for(int i=0;i<qtastro;i++){
-						string t_cpf = RegistroAstronautas.getE(i)->getCPF();
-						bool t_vivo = RegistroAstronautas.getE(i)->getVivo();
-						cout << setw(tamax/2) << left << ""
-							 << setw(tamax+(tamax/2)) << left << RegistroAstronautas.getE(i)->getNome()
-							 << setw((tampadrao/2)) << left << "||"
-							 << setw(cpftamanho+(tampadrao/2)) << left << formatCPF(t_cpf)
-							 << setw((tampadrao/2)) << left << "||"
-							 << setw((tampadrao/2)) << left << RegistroAstronautas.getE(i)->getIdade()
-							 << setw((tampadrao/2)) << left << "||";
-
-						//Se o astronauta estiver vivo, imprime que ele está vivo na coluna "Situação".
-						if(t_vivo){
-							cout << setw((tampadrao/2)+3) << left << "Vivo(a)"
-								 << setw((tampadrao/2)) << left << "||"
-								 << endl;
-						} else{ //Caso esteja morto (vivo = false), imprime que ele está morto na coluna "Situação".
-							cout << setw((tampadrao/2)+3) << left << "Morto(a)"
-								 << setw((tampadrao/2)) << right << "||"
-								 << endl;
-						}
+				//Loop para que haja a comparação entre os tamanhos de cada elemento do vetor, pra no fim "tamax" carregar o tamanho do maior nome.
+				for(int i=0;i<qtastro;i++){
+					text = RegistroAstronautas.getE(i)->getNome();
+					temptam = text.length();
+					if(tamax < temptam){
+						tamax = temptam;
 					}
 				}
-				cout << endl; //Pula linha
-				break;
 
-			case 3:
-				RegistroVoos.addE(addVoo());
-				cout << endl;
-				break;
+				int cpftamanho = 14; //Variável que carrega o tamanho máximo de um CPF já formatado.
+				int tampadrao = 16; //Variável que carrega o tamanho padrão (tamanho do cpf + 2 para que haja um pequeno espaçamento (2)).
+				int tamtotal = (tamax-2) + tamax + cpftamanho + (cpftamanho/2) + 3*tampadrao; //Tamanho total da largura da tabela.
 
-			case 4:
-				cout << "Voos:\n" << endl;
+				//Nomes das colunas.
+				cout << setw(tamax-2) << left << ""
+					 << setw(tamax+cpftamanho) << left << "Nome"
+					 << setw(tampadrao+(cpftamanho/2)) << left << "CPF"
+					 << setw(tampadrao) << left << "Idade"
+					 << setw(tampadrao) << left << "Situação"
+					 << endl;
 
-				for(int i=0;i<qt_voos;i++){
-					cout << "Código do voo " << i+1 << ": "
-						<< RegistroVoos.getE(i)->getCode()
-						<< endl;
+				//Linha para separar.
+				cout << setfill('-') << setw(tamtotal) << "-" << endl;
+				cout << setfill(' ');
+
+				//Print dos valores de cada astronauta.
+				for(int i=0;i<qtastro;i++){
+					string t_cpf = RegistroAstronautas.getE(i)->getCPF();
+					bool t_vivo = RegistroAstronautas.getE(i)->getVivo();
+					cout << setw(tamax/2) << left << ""
+						 << setw(tamax+(tamax/2)) << left << RegistroAstronautas.getE(i)->getNome()
+						 << setw((tampadrao/2)) << left << "||"
+						 << setw(cpftamanho+(tampadrao/2)) << left << formatCPF(t_cpf)
+						 << setw((tampadrao/2)) << left << "||"
+						 << setw((tampadrao/2)) << left << RegistroAstronautas.getE(i)->getIdade()
+						 << setw((tampadrao/2)) << left << "||";
+
+					//Se o astronauta estiver vivo, imprime que ele está vivo na coluna "Situação".
+					if(t_vivo){
+						cout << setw((tampadrao/2)+3) << left << "Vivo(a)"
+							 << setw((tampadrao/2)) << left << "||"
+							 << endl;
+					} else{ //Caso esteja morto (vivo = false), imprime que ele está morto na coluna "Situação".
+						cout << setw((tampadrao/2)+3) << left << "Morto(a)"
+							 << setw((tampadrao/2)) << right << "||"
+							 << endl;
+					}
 				}
+			}
+			cout << endl; //Pula linha
+		} else if(ans == 3){
+			RegistroVoos.addE(addVoo());
+			cout << endl;
+		} else if(ans == 4){
+			cout << "Voos:\n" << endl;
 
-				cout << endl;
-				break;
-			
-			case 5:
-				int temp_code;
-				string temp_cpf;
+			for(int i=0;i<qt_voos;i++){
+				cout << "Código do voo " << i+1 << ": "
+					<< RegistroVoos.getE(i)->getCode()
+					<< endl;
+			}
 
-				LimparBuffer();
+			cout << endl;
+		} else if(ans == 5){
+			int temp_code;
+			string temp_cpf;
 
-				cout << "Digite o voo que será adicionado o tripulante:" << endl;
-				cin >> temp_code;
+			cout << "Digite o voo que será adicionado o tripulante:" << endl;
+			cin >> temp_code;
 
-				cout << "Digite o CPF do astronauta que será adicionado na tripulação" << endl;
-				getline(cin, temp_cpf);
+			LimparBuffer();
 
-				break;
-			
-			case 0:
-				ans = 0;
-				break;
+			cout << "Digite o CPF do astronauta que será adicionado na tripulação" << endl;
+			getline(cin, temp_cpf);
 
-			default:
-				break;
+			Astronauta* cpf_astro = getAstroByCPF(temp_cpf, RegistroAstronautas);
+			Voo* code_voo = getVooByCode(temp_code, RegistroVoos);
+
+			addTripulante(code_voo, cpf_astro);
 		}
 	}
 
